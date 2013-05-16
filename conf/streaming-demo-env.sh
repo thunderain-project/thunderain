aa2b172fa69d68fa8a61dafedff4c8324eb4ccd8
jerryshao <saisai.shao@intel.com>
2013-05-16 14:05:27 +0800

    change reduceByKey to zipPartition in weblog output

--- conf/streaming-demo-env.sh
+++ conf/streaming-demo-env.sh
@@ -4,13 +4,8 @@
 export SPARK_MASTER_URL=local[10]
 export SPARK_HOME=/home/jerryshao/source-code/spark-0.7.0/
 export STREAM_JAR_PATH=/home/jerryshao/source-code/streaming-demo/target/scala-2.9.2/streaming-demo_2.9.2-0.0.1.jar
-
-export OUTPUT_PARTITION_NUM=16
-
+export DATA_CLEAN_TTL=600
 # kafka configuration
 export ZK_QUORUM=localhost:2181
 export KAFKA_GROUP=test
 export KAFKA_INPUT_NUM=4
-# tachyon configuration
-export TACHYON_MASTER=localhost:19998
-export TACHYON_WAREHOUSE_PATH=/user/tachyon
--- src/main/scala/example/weblog/output/WebLogOutput.scala
+++ src/main/scala/example/weblog/output/WebLogOutput.scala
@@ -22,6 +22,7 @@ class WebLogOutput extends AbstractEventOutput {
   private var outputName: String = _
   private var jobName: String = _
   val PART_NUM = Some(System.getenv("OUTPUT_PARTITION_NUM")).getOrElse("4").toInt
+  val cleanBefore = Some(System.getenv("DATA_CLEAN_TTL")).getOrElse("600").toInt
   private var types: Array[String] = _
   
   override def setOutputName(name: String) {
@@ -39,85 +40,94 @@ class WebLogOutput extends AbstractEventOutput {
     stream.foreach(r => {      
       val statAccum = SharkEnv.sc.accumulableCollection(mutable.ArrayBuffer[(Int, TableStats)]())
       
-      val newRdd = r.mapPartitionsWithIndex((index, iter) => {
-        val colBuilders = types.map(ColumnBuilderFactory.newColumnBuilder(_))
-        val objInspectors: Array[ObjectInspector] = types.map(
-            PrimitiveObjInspectorFactory.newPrimitiveObjInspector(_))
-        
-        var numRows = 0;
-        val currTime = (System.currentTimeMillis() / 1000).asInstanceOf[Object];
-        
-        iter.foreach(row => {
-          val (t1, ((t2, t3), t4)) = (currTime, row.asInstanceOf[((Object, Object), Object)])
-          Array(t1, t2, t3, t4).zipWithIndex.foreach(r => 
-            colBuilders(r._2).append(r._1, objInspectors(r._2)))
-
-          numRows += 1
-        })
-        
-        val columns = colBuilders.map(_.build)
-        val tableStats = new TableStats(
-          columns.map { c => c.stats match {
-            case s: ColumnNoStats[_] => None
-            case s: ColumnStats[_] => Some(s)
-        }}, numRows)
-        
-        statAccum += (index, tableStats)
-        
-        Iterator(new TableStorage(numRows, columns.map(_.format)))
+      val tblRdd = SharkEnv.cache.get(outputName) match {
+        case None => buildTableRdd(r, statAccum)
+        case Some(s) => zipTableRdd(s.asInstanceOf[RDD[TableStorage]], r, statAccum)
+      }
+      tblRdd.foreach(_ => Unit)
+         
+      // put rdd and statAccum to cache manager
+      SharkEnv.cache.put(outputName, tblRdd, StorageLevel.MEMORY_ONLY)
+      SharkEnv.cache.putStats(outputName, statAccum.value.toMap)
+    })
+  }
+  
+  private def buildTableRdd(rdd: RDD[_], 
+    stat: spark.Accumulable[mutable.ArrayBuffer[(Int, TableStats)],(Int, TableStats)]) 
+  = {
+    val newRdd = rdd.mapPartitionsWithIndex((index, iter) => {
+      val colBuilders = types.map(ColumnBuilderFactory.newColumnBuilder(_))
+      val objInspectors: Array[ObjectInspector] = types.map(
+          PrimitiveObjInspectorFactory.newPrimitiveObjInspector(_))
+      
+      var numRows = 0;
+      val currTime = (System.currentTimeMillis() / 1000).asInstanceOf[Object];
+      
+      iter.foreach(row => {
+        val (t1, ((t2, t3), t4)) = (currTime, row.asInstanceOf[((Object, Object), Object)])
+        Array(t1, t2, t3, t4).zipWithIndex.foreach(r => 
+          colBuilders(r._2).append(r._1, objInspectors(r._2)))
+        numRows += 1
       })
       
-      // union current RDD and previous RDD to a new RDD, put to cache
-      val unionRdd = SharkEnv.cache.get(outputName) match {
-        case None => newRdd
-        case Some(r) => r.asInstanceOf[RDD[TableStorage]].union(newRdd)
-      }
-
-      //reduce the union RDD for better performance
-      var i = 0l
-      val reducedRdd = unionRdd.map(r => {i += 1; (i, r)}).reduceByKey((s1, s2) => {
+      val columns = colBuilders.map(_.build)
+      val tableStats = new TableStats(
+        columns.map { c => c.stats match {
+          case s: ColumnNoStats[_] => None
+          case s: ColumnStats[_] => Some(s)
+      }}, numRows)
+      
+      stat += (index, tableStats)      
+      Iterator(new TableStorage(numRows, columns.map(_.format)))
+    })
+    
+    newRdd
+  }
+  
+  private def zipTableRdd(oldRdd: RDD[_], newRdd: RDD[_],
+    stat: spark.Accumulable[mutable.ArrayBuffer[(Int, TableStats)],(Int, TableStats)]) = {
+    val tblRdd = buildTableRdd(newRdd, stat)
+    val zippedRdd = oldRdd.asInstanceOf[RDD[TableStorage]].zipPartitions(
+      (i1: Iterator[TableStorage], i2: Iterator[TableStorage]) => {
         val colBuilders = types.map(ColumnBuilderFactory.newColumnBuilder(_))
         val objInspectors: Array[ObjectInspector] = types.map(
             PrimitiveObjInspectorFactory.newPrimitiveObjInspector(_))
-        val storages = Array(s1, s2)
         var numRows = 0
-        storages.foreach(r => {
-          r.iterator.foreach(c => {
-        	val row = c.getFieldsAsList()
+        val currTm = System.currentTimeMillis() / 1000
+        
+        Array(i1, i2).foreach(r => {
+          r.foreach(t => { 
+            t.iterator.foreach(c => {
+        	    val row = c.getFieldsAsList()
+        	    val tm = row.get(0).asInstanceOf[LongWritable].get
+      
+        	    if (row.get(1) != null && row.get(2) != null && (currTm - tm) < cleanBefore) {
+                colBuilders(0).append(
+                  row.get(0).asInstanceOf[LongWritable].get: java.lang.Long, objInspectors(0))
+                colBuilders(1).append(
+                  row.get(1).asInstanceOf[Text].toString, objInspectors(1))
             
-        	if (row.get(1) != null && row.get(2) != null) {
-              colBuilders(0).append(
-                row.get(0).asInstanceOf[LongWritable].get: java.lang.Long, objInspectors(0))
-              colBuilders(1).append(
-                row.get(1).asInstanceOf[Text].toString, objInspectors(1))
-            
-              jobName match {
-        	    case "item_view" => 
-        	      colBuilders(2).append(
-        	        row.get(2).asInstanceOf[LongWritable].get: java.lang.Long, objInspectors(2))
-        	    case "subcategory_view" => 
-        	      colBuilders(2).append(
-        	        row.get(2).asInstanceOf[Text].toString, objInspectors(2))
-        	    case _ => throw new Exception("unkown job name " + jobName)
-        	  }
-              colBuilders(3).append(
-                row.get(3).asInstanceOf[LongWritable].get: java.lang.Long, objInspectors(3))
-              numRows += 1
-        	}
+                jobName match {
+        	      case "item_view" => 
+        	        colBuilders(2).append(
+        	          row.get(2).asInstanceOf[LongWritable].get: java.lang.Long, objInspectors(2))
+        	      case "subcategory_view" => 
+        	        colBuilders(2).append(
+        	          row.get(2).asInstanceOf[Text].toString, objInspectors(2))
+        	      case _ => throw new Exception("unkown job name " + jobName)
+        	      }
+                colBuilders(3).append(
+                  row.get(3).asInstanceOf[LongWritable].get: java.lang.Long, objInspectors(3))
+                numRows += 1
+        	    }
+            })
           })
         })
         
         val columns = colBuilders.map(_.build)
+        Iterator(new TableStorage(numRows, columns.map(_.format)))
+      }, tblRdd)
         
-        new TableStorage(numRows, columns.map(_.format))
-      }, PART_NUM).map(r => r._2)
-      
-      reducedRdd.foreach(_ => Unit)
-      
-      // put rdd and statAccum to cache manager
-      SharkEnv.cache.put(outputName, reducedRdd, StorageLevel.MEMORY_ONLY)
-      SharkEnv.cache.putStats(outputName, statAccum.value.toMap)
-
-    })
+    zippedRdd
   }
 }
